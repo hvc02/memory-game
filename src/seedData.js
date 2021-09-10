47ab0ae816aea5219758036b542fd59b937a202b
harivansh <harichanda02@gmail.com>
2021-09-11 00:13:56 +0530

    seed data added

---
+++ src/seedData.js
@@ -0,0 +1,50 @@
+import cristiano from "./assets/images/cristiano.jpg";
+import lewandoski from "./assets/images/lewandoski.jpg";
+import messi from "./assets/images/messi.jpg";
+import mbappe from "./assets/images/mbappe.jpg";
+import bruno from "./assets/images/bruno.jpg";
+import modric from "./assets/images/modric.jpg";
+import haaland from "./assets/images/haaland.jpg";
+import son from "./assets/images/son.jpg";
+
+export const data = [
+  {
+    type: "cristiano",
+    img: cristiano,
+  },
+
+  {
+    type: "lewandoski",
+    img: lewandoski,
+  },
+
+  {
+    type: "messi",
+    img: messi,
+  },
+
+  {
+    type: "mbappe",
+    img: mbappe,
+  },
+
+  {
+    type: "bruno",
+    img: bruno,
+  },
+
+  {
+    type: "modric",
+    img: modric,
+  },
+
+  {
+    type: "haaland",
+    img: haaland,
+  },
+
+  {
+    type: "son",
+    img: son,
+  },
+];
