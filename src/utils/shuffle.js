6b9eff548c3293b93cba11cf4a0129b5a6c2ea60
harivansh <harichanda02@gmail.com>
2021-09-11 00:13:44 +0530

    shuffle method added

---
+++ src/utils/shuffle.js
@@ -0,0 +1,15 @@
+function swap(array, currentIndex, randomIndex) {
+  const temp = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temp;
+}
+
+export function shuffle(array) {
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    swap(array, currentIndex, randomIndex);
+  }
+  return array;
+}
